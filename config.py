01ed254b2ce90df33ede4c21142957c1337670ad
emmanueliarussi <emmanueliarussi@gmail.com>
2020-06-03 19:11:12 -0300

    first commit

---
+++ config.py
@@ -0,0 +1,46 @@
+# tweepy-bots/bots/config.py
+import tweepy
+import logging
+import os
+from os import environ
+import telegram
+from telegram.error import NetworkError, Unauthorized
+import requests
+import re
+
+logger = logging.getLogger()
+
+# Variables del ambiente con las claves de autoenticacion
+CONSUMER_KEY = environ['CONSUMER_KEY']
+CONSUMER_SECRET = environ['CONSUMER_SECRET']
+ACCESS_TOKEN = environ['ACCESS_TOKEN']
+ACCESS_TOKEN_SECRET = environ['ACCESS_TOKEN_SECRET']
+TELEGRAM_KEY = environ['TELEGRAM_KEY']
+
+def create_api():
+    # Autenticación a Twitter
+    auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+    auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+    api = tweepy.API(auth)
+
+    # Log for debug
+    #logger.info("CONSUMER_KEY = {}".format(CONSUMER_KEY))
+    #logger.info("CONSUMER_SECRET = {}".format(CONSUMER_SECRET))
+    #logger.info("ACCESS_TOKEN = {}".format(ACCESS_TOKEN))
+    #logger.info("ACCESS_TOKEN_SECRET = {}".format(ACCESS_TOKEN_SECRET))
+    #logger.info("TELEGRAM_KEY = {}".format(TELEGRAM_KEY))
+
+    # Autenticación a Telegram
+    bot = telegram.Bot(TELEGRAM_KEY)
+    logger.info("API de Telegram lista!")
+
+    try:
+        api.verify_credentials()
+    except Exception as e:
+        logger.error("Error de verificacion en la API de Twitter", exc_info=True)
+        raise e
+    logger.info("API de Twitter lista!")
+
+    return api,bot
+
+
---
+++ server.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python
+
+from os import environ
+from flask import Flask
+import tweepy
+import logging
+from config import create_api
+import json
+import time
+import telegram
+from telegram.error import NetworkError, Unauthorized
+from time import sleep
+import requests
+import re
+import urllib.request
+
+# Log
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger()
+
+# Tiempo de espera entre updates
+sleep_time_sec = 5 
+
+update_id = None
+link      = None
+
+# Patron de los links de Twitter 
+url_pattern = re.compile("https://twitter.com/([a-zA-Z0-9_]+)/status/([0-9_]+).*")
+
+# Obtener link desde el bot de telegram
+def getLink(bot):
+
+    global update_id
+
+    # Obtener ultimos updates del bot
+    for update in bot.get_updates(offset=update_id, timeout=10):
+        update_id = update.update_id + 1
+        # Verifico que el mensaje contenga texto (podrían ser de otro tipo los updates)
+        if update.message:  
+            if update.message.text:
+                if url_pattern.match(update.message.text):
+                    try:
+                        html = urllib.request.urlopen(update.message.text)
+                        logger.info(update.message.text)
+                        # Respuesta automatica para el usuario que envio el link
+                        update.message.reply_text("Con tus impuestos twiteamos el link. Gracias!")
+                        # Retorno el link
+                        return update.message.text
+                    except:
+                        logger.info("Link inválido")
+                        update.message.reply_text("El link no es válido, la dirección del tweet no existe.")
+                        return None 
+                else:
+                    logger.info("Link inválido")
+                    update.message.reply_text("El link no es válido. Recordá enviar links a tweets que comiencen con 'https://twitter.com/'")
+                    return None 
+
+    # Si no hay nada, retorno None
+    return None
+
+# Twitear un link
+def tweet_link(api,tweet_ref):
+    # Log 
+    logger.info(f"Procesando el tweet..") 
+    logger.info(tweet_ref)
+    # Tweet "Con tus impuestos [LINK]"
+    api.update_status("Con tus impuestos {}".format(tweet_ref))
+    logger.info(f"Twiteado!")
+
+# Main del bot
+def index():
+    logger.info(f"Bienvenido a @contusimpuestos BOT") 
+    global update_id
+    global link
+    
+    # Init APIs de Telegram y Twitter
+    api,telegramBot = create_api()
+
+    # Loop del bot
+    while True:
+        # Chequear si hay links disponibles para twitear
+        try:
+            link = getLink(telegramBot)
+        except NetworkError:
+            sleep(1)
+        except Unauthorized:
+            update_id += 1
+
+        # Si hay link...
+        if(link != None):
+            # Lo twiteamos!
+            tweet_link(api,link)
+            link = None
+        
+        # Espero un poco antes de volver a chequear
+        logger.info("...")
+        time.sleep(sleep_time_sec)
+
+    return 0
+        
+if __name__ == "__main__":
+    index()
+
+    
+
